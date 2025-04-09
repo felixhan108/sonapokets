069569f4ab2730a3174c809de1cb0847d1b89e72
HanSungSoo <thepub03@gmail.com>
2025-04-09 23:52:29 +0900

    이렇게 다시 해보자

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,10 +12,6 @@ docker-compose pull
 # 2. photoprism과 nginx 서비스만 먼저 시작합니다
 docker-compose up -d photoprism nginx
 
-# 3. photoprism이 준비될 때까지 기다립니다 (예: 30초)
-echo "PhotoPrism이 시작되기를 기다리는 중..."
-sleep 30
-
 # npm run build가 캐쉬로 인해 계속 반영 되지 않아 (CACHEBUST=$(date +%s)을 이용해 캐쉬 사용을 불가능하게 변경)
 docker-compose build frontend --build-arg CACHEBUST=$(date +%s)
 docker-compose up -d frontend

c0526d9fd8e7160eb06dfeb1fa6b34dd06c11b5b
HanSungSoo <thepub03@gmail.com>
2025-04-24 04:36:23 +0900

    1

--- fantasyavatar/src/main/components/MainScreen.tsx
+++ fantasyavatar/src/main/components/MainScreen.tsx
@@ -9,9 +9,8 @@ export default function MainScreen() {
   useEffect(() => {
     // 메인 씬 클래스 정의
     class MainScene extends Phaser.Scene {
-      soldier!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-      cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-
+      private soldier!: Phaser.GameObjects.Sprite;
+      private background!: Phaser.GameObjects.TileSprite;
       constructor() {
         // 생성자에서 씬 이름 설정
         super('MainScene');
@@ -19,12 +18,18 @@ export default function MainScreen() {
 
       // 로드 씬 정의
       preload() {
-        // 캐릭터 이미지 로드
+        // 배경 불러오기
+        this.load.image('tiles', 'assets/bg/nature.png');
+        this.load.tilemapTiledJSON('map', 'assets/bg/river.json');
+        this.load.image('background', 'assets/bg/nature.png');
+
+        // 캐릭터 - Idle
         this.load.spritesheet('soldier-idle', '/assets/sprites/Soldier-Idle.png', {
           frameWidth: 100, // 디자인에 정해놓은 그리드 사이즈
           frameHeight: 100,
         });
 
+        // 캐릭터 - Walk
         this.load.spritesheet('soldier-walk', '/assets/sprites/Soldier-Walk.png', {
           frameWidth: 100, // 디자인에 정해놓은 그리드 사이즈
           frameHeight: 100,
@@ -33,6 +38,15 @@ export default function MainScreen() {
 
       // 생성 씬 정의
       create() {
+        /**
+         * 배경 생성하기
+         */
+
+        var map = this.make.tilemap({ key: 'map' });
+        const tiles = map.addTilesetImage('river', 'tiles')!;
+        const layer = map.createLayer(0, tiles, 0, 0);
+        // this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'tiles').setOrigin(0, 0);
+
         this.anims.create({
           key: 'idle',
           frames: this.anims.generateFrameNumbers('soldier-idle', { start: 0, end: 5 }),
@@ -47,33 +61,28 @@ export default function MainScreen() {
           repeat: -1, // 무한 반복
         });
 
-        // 물리 시스템으로 캐릭터 생성
-        this.soldier = this.physics.add.sprite(100, 150, 'soldier-idle');
-        this.soldier.setScale(2);
+        this.soldier = this.add.sprite(50, 60, 'soldier-idle');
         this.soldier.play('idle');
 
-        // 1초 뒤에 run 애니메이션으로 변경 + 속도 증가
+        // 1초 뒤에 walk 애니메이션으로 변경 + 속도 증가
         this.time.delayedCall(1000, () => {
           this.soldier.play('walk');
         });
       }
 
-      update() {}
+      update() {
+        // this.background.tilePositionX += 1;
+      }
     }
 
     const config: Phaser.Types.Core.GameConfig = {
-      type: Phaser.AUTO, // 게임 타입 설정
-      width: 400, // 게임 너비 설정
-      height: 300, // 게임 높이 설정
+      type: Phaser.WEBGL, // 게임 타입 설정
+      width: 10 * 16, // 게임 너비 설정
+      height: 10 * 16, // 게임 높이 설정
       parent: phaserRef.current, // 게임 컨테이너 설정
       scene: MainScene, // 메인 씬 설정
       pixelArt: true, // 픽셀 아트 효과 활성화
-      physics: {
-        default: 'arcade', // 물리 엔진 설정
-        arcade: {
-          gravity: { x: 0, y: 0 }, // 중력 설정
-        },
-      },
+      zoom: 2,
     };
 
     // 게임 인스턴스 생성
